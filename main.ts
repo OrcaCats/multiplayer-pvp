5ca1e2436e66ed67748c4df7072c5b5600e86e98
LinearTechnologies <124675744+LinearTechnologies@users.noreply.github.com>
2023-11-27 13:21:07 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function EmemyMovement (X: number, Y: number, Enemy: game.LedSprite) {
+	
+}
+function Sync_A_Sync (Enemy: game.LedSprite, X: number, Y: number) {
+	
+}
 input.onButtonPressed(Button.A, function () {
     sprite.move(1)
     radio.sendValue("Move", 1)
@@ -7,11 +13,22 @@ input.onButtonPressed(Button.B, function () {
     radio.sendValue("Turn", 90)
 })
 input.onGesture(Gesture.Shake, function () {
-    radio.sendValue("init", 0)
+    PlayerAB = randint(0, 1)
+    radio.sendValue("init", PlayerAB)
+    if (PlayerAB == 0) {
+        sprite = game.createSprite(0, 4)
+    } else {
+        sprite = game.createSprite(4, 0)
+    }
 })
 radio.onReceivedValue(function (name, value) {
+    let Enemy: game.LedSprite = null
     if (name == "init") {
-        Enemy = game.createSprite(4, 2)
+        if (value == 0) {
+            SelfStartX = 0
+        } else {
+            SelfStartX = 4
+        }
     }
     if (name == "Move") {
         Enemy.move(1)
@@ -20,7 +37,7 @@ radio.onReceivedValue(function (name, value) {
         Enemy.turn(Direction.Right, 90)
     }
 })
-let Enemy: game.LedSprite = null
+let SelfStartX = 0
+let PlayerAB = 0
 let sprite: game.LedSprite = null
-sprite = game.createSprite(0, 2)
 radio.setGroup(69)
